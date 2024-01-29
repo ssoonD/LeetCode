ef9e694c5f65b6d2fef9e39c402718b7467a996b
ssoonD <sxxnweek@gmail.com>
2024-01-29 20:22:56 +0900

    Time: 270 ms (5.30%), Space: 45.2 MB (54.43%) - LeetHub

--- 0931-minimum-falling-path-sum/0931-minimum-falling-path-sum.java
+++ 0931-minimum-falling-path-sum/0931-minimum-falling-path-sum.java
@@ -1,4 +1,5 @@
 class Solution {
+    public static final int MAX = 10001;
     int[][] memo;
     
     public int minFallingPathSum(int[][] matrix) {
@@ -10,15 +11,15 @@ class Solution {
         return solve(len);
     }
     
+    // 초기화
     public void init(int len) {
         memo = new int[len][len];
-        for(int i = 0; i< len; i++) {
-            for(int j = 0; j < len; j++) {
-                memo[i][j] = 10001;
-            }
+        for (int i = 0; i < len; i++) {
+            Arrays.fill(memo[i], MAX);
         }
     }
     
+    // dp
     public void dp(int[][] matrix, int len, int row, int col, int sum) {
         // 범위 초과 시 종료
         if(row < 0 || row >= len || col < 0 || col >= len) {
@@ -26,9 +27,9 @@ class Solution {
         }
         
         // memo에 저장한 값보다 작은 경우 바꿔주기
-        int minSum = sum + matrix[row][col];
-        if(minSum < memo[row][col]) { // 작은 경우 저장
-            memo[row][col] = minSum;
+        int checkSum = sum + matrix[row][col];
+        if(checkSum < memo[row][col]) { // 작은 경우 저장
+            memo[row][col] = checkSum;
         } else { // 큰 경우 return
             return;
         }
@@ -38,8 +39,9 @@ class Solution {
         dp(matrix, len, row + 1, col + 1, memo[row][col]);
     }
     
+    // 최소값 찾기
     public int solve(int len) {
-        int result = 10001;
+        int result = MAX;
         for(int i = 0; i < len; i++) {
             if(memo[len-1][i] < result) {
                 result = memo[len-1][i];
